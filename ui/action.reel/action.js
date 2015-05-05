7a692cec42214424cf8c2b264501ac44c9584ad8
DIRODKD <noreply>
2015-05-05 13:40:36 +0000

    Update files

---
+++ ui/action.reel/action.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/action.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Action
+ * @extends Component
+ */
+exports.Action = Component.specialize(/** @lends Action# */ {
+    constructor: {
+        value: function Action() {
+            this.super();
+        }
+    }
+});
